5225271303481f22f13fda9fc50d261f945ad903
Jesse Johnson <jessejohnson100@yahoo.com>
2017-01-25 19:05:27 +0000

    Second Users Test

---
+++ app/models/micropost.rb
@@ -0,0 +1,12 @@
+class Micropost < ApplicationRecord
+  validates :content, length: { maximum: 140 }
+end
+class Micropost < ApplicationRecord
+  belongs_to :user
+  validates :content, length: { maximum: 140 }
+end
+class Micropost < ApplicationRecord
+  belongs_to :user
+  validates :content, length: { maximum: 140 },
+                      presence: true
+end
